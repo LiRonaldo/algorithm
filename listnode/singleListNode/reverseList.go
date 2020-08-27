42e51f0afc207ef7d52cd74cc1432ff9eded54a1
yuxiangli8@126.com <yuxiangli8@126.com>
2020-08-27 21:11:30 +0800

    单链表反转，递归

--- listnode/singleListNode/reverseList.go
+++ listnode/singleListNode/reverseList.go
@@ -12,9 +12,9 @@ type ListNode struct {
 
 func main() {
 	head := &ListNode{}
-	createListNode(10, head)
+	createListNode(3, head)
 	sout(head)
-	pre := reverseList(head)
+	pre := reverse(head)
 	sout(pre)
 }
 func createListNode(n int, node *ListNode) {
@@ -33,6 +33,7 @@ func sout(head *ListNode) {
 	fmt.Println("")
 }
 
+//普通反转,比较不好理解
 func reverseList(head *ListNode) *ListNode {
 	cur := head
 	var pre *ListNode
@@ -40,4 +41,16 @@ func reverseList(head *ListNode) *ListNode {
 		cur.next, pre, cur = pre, cur, cur.next
 	}
 	return pre
+}
+
+//递归反转，还比较好理解。递归理解成栈，先进后出
+func reverse(head *ListNode) *ListNode {
+	if head.next == nil || head == nil {
+		return head
+	}
+	//一直递归找到最后一个元素，则当前的head 就是倒数第二个元素。把第二个放到最后。然后，next=nil
+	last := reverse(head.next)
+	head.next.next = head
+	head.next = nil
+	return last
 }
