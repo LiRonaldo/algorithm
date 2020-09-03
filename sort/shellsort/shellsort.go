d529808b4cd7ba81a16f503324c3e83fb3241d9b
yuxiangli8@126.com <yuxiangli8@126.com>
2020-09-03 16:19:46 +0800

    改错希尔排序，希尔排序就是一个增量的插入排序，不是每个元素做插入排序，而是没赠量个元素做插入排序。

--- sort/shellsort/shellsort.go
+++ sort/shellsort/shellsort.go
@@ -4,6 +4,10 @@ import "fmt"
 
 /**
 希尔排序 ,就是分组的插入排序。只要掌握插入排序。然后 i++的时候要i+分组的
+确定一个增量一般是n=n/2
+总得思路是： 假设len（arr）=6,那么n=3,
+相当于普通的插入排序，但是和普通的插入排序不一样，普通的插入排序是i++,希尔排序是i+n,
+也就事原数组 a[0],a[3]a[0] 他们三个元素做插入排序。
 */
 func main() {
 	arr := []int{7, 3, 4, 8, 18, 1}
@@ -12,17 +16,17 @@ func main() {
 }
 func sort(arr []int) {
 	n := len(arr)
-	for n > 1 {
+	for n > 0 {
+		//确定增量,增量每次都/2.
 		n = n / 2
-		for i := 0; i < n; i++ {
-			for j := n; j < len(arr); j = j + n {
-				var temp = arr[j]
-				var k int
-				for k = j; k > 0 && arr[k-n] > temp; k = k - n {
-					arr[k] = arr[k-n]
-				}
-				arr[k] = temp
+		//相当于插入排序
+		for i := n; i < len(arr); i++ {
+			var temp = arr[i]
+			var k int
+			for k = i - n; k >= 0 && arr[k] > temp; k = k - n {
+				arr[k+n] = arr[k]
 			}
+			arr[k+n] = temp
 		}
 	}
 }
