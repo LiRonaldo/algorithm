9380a1a152f5c530a415e464754d44d8c2dae367
yuxiangli8@126.com <yuxiangli8@126.com>
2020-09-16 22:22:20 +0800

    斐波那契 优化

--- leecode/dynamicplanning/stairway.go
+++ leecode/dynamicplanning/stairway.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /**
 有一座高度是10级台阶的楼梯，从下往上走，每跨一步只能向上1级或者2级台阶。要求用程序来求出一共有多少种走法。
@@ -17,6 +19,8 @@ soul2方法是对斐波那契数列的优化
 func main() {
 	num := soul1(10)
 	fmt.Println(num)
+	num1 := soul2(10)
+	fmt.Println(num1)
 }
 
 /**
@@ -30,4 +34,21 @@ func soul1(n int) int {
 		return 2
 	}
 	return soul1(n-1) + soul1(n-2)
+}
+
+/**
+优化斐波那契
+a    b    tmp
+1    2    3      5
+*/
+func soul2(n int) int {
+	a := 1
+	b := 2
+	tmp := 0
+	for i := 2; i < n; i++ {
+		tmp = a + b
+		a = b
+		b = tmp
+	}
+	return tmp
 }
